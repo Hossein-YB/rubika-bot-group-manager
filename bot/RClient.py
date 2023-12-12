68ae72d9a47473f21c69725670d1e5cb9b000f03
Hossein <hosseinyaghoubi919@gmail.com>
2023-12-12 13:30:12 +0330

    create set_sudo function for set bot sudo admin

--- bot/RClient.py
+++ bot/RClient.py
@@ -1,19 +1,37 @@
+# rubpy imports
 from rubpy import Client, handlers, Message
 from rubpy.structs import models
-import asyncio
+
+# database models imports
+from db.models import Admin, Group, GroupAdmin
 
 
 class RubikaBot(Client):
     def __init__(self, session: str, *args, **kwargs):
-        self.name = "__name__"
+        self.sudo = Admin.get_sudo()
+        self.groups_id = []
         super().__init__(session, *args, **kwargs)
 
     async def check_bot(self, msg: Message):
         print(msg)
         await msg.reply(f"Hi {self.name} bot is online")
 
+    async def set_sudo_admin(self, msg: Message):
+        guid = msg.author_guid
+        if self.sudo:
+            ad = await self.get_user_info(self.sudo)
+            return await msg.reply(f"مدیر از قبل تنظیم شده است\n{ad.user.username}")
+
+        if Admin.insert_admin(guid):
+            self.sudo = guid
+            await msg.reply("مدیر تنظیم شد.")
+        else:
+            await msg.reply("مشکلی رخ داد.")
+        
+
     async def run_until_disconnected(self):
         await self.start()
-        self.add_handler(self.check_bot, handlers.MessageUpdates(models.is_private()))
+        # self.add_handler(self.check_bot, handlers.MessageUpdates(models.is_private()))
+        self.add_handler(self.set_sudo_admin, handlers.MessageUpdates(models.RegexModel('^!setsudo$') and models.is_private()))
         return await super().run_until_disconnected()
 
--- db/models.py
+++ db/models.py
@@ -11,6 +11,23 @@ class BaseModel(Model):
 class Admin(BaseModel):
     guid = CharField(max_length=250, unique=True)
 
+    @classmethod
+    def insert_admin(cls, guid):
+        user = cls.get_or_none(cls.guid == guid)
+        if not user and guid:
+            cls.create(guid=guid)
+            return True
+        else:
+            return False
+
+    @classmethod
+    def get_sudo(cls):
+        user = cls.get_or_none()
+        if user:
+            return user.guid
+        else:
+            return None
+
 
 class Group(BaseModel):
     guid = CharField(max_length=250, unique=True)
