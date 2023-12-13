e5b6b1fdeccd56335dffb4d7da200c44cc2dbe28
Hossein <hosseinyaghoubi919@gmail.com>
2023-12-13 15:01:12 +0330

    insert group admins and get them

--- db/models.py
+++ db/models.py
@@ -1,7 +1,8 @@
-from peewee import SqliteDatabase, Model, CharField, ForeignKeyField
+from pdb import set_trace
+
+from peewee import SqliteDatabase, Model, CharField, ForeignKeyField, BooleanField
 
 database = SqliteDatabase("bot_db.sqlite3")
-
 
 class BaseModel(Model):
     class Meta:
@@ -52,7 +53,35 @@ class Group(BaseModel):
         else:
             return groups_ids
 
+
 class GroupAdmin(BaseModel):
     guid = CharField(max_length=250, unique=True)
-    group_id = ForeignKeyField(Group, on_delete="CASCADE")
+    is_sudo = BooleanField(default=False)
+    group_id = ForeignKeyField(Group, Group.guid, on_delete="CASCADE")
 
+    @classmethod
+    def insert_group_admin(cls, a_guid, g_guid, is_sudo=False):
+        group = cls.get_or_none(cls.guid == a_guid, cls.group_id == g_guid)
+        if not group and a_guid and g_guid:
+            cls.create(guid=a_guid, group_id=g_guid, is_sudo=is_sudo)
+            return True
+        else:
+            return False
+
+    @classmethod
+    def get_groups_admins_list(cls):
+        admins = cls.select()
+        groups_admin = dict()
+        if admins:
+            set_trace()
+            for admin in admins:
+                if not groups_admin.get(admin.group_id.guid, None):
+                    groups_admin[admin.group_id.guid] = {'admins': [], "creator": ""}
+
+                groups_admin[admin.group_id.guid]['admins'].append(admin.guid)
+                if admin.is_sudo:
+                    groups_admin[admin.group_id.guid]['creator'] = admin.guid
+
+            return groups_admin
+        else:
+            return groups_admin
