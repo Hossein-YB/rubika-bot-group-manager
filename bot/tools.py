1ba12abe2677dbec432217719e7f373281b73945
Hossein <hosseinyaghoubi919@gmail.com>
2024-10-16 22:03:29 +0330

    update tools and mention re and error writer

--- bot/tools.py
+++ bot/tools.py
@@ -1,7 +1,12 @@
+import datetime
 import re
-from time import sleep
 
-LINK_RE = r"((http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?|([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?)"
-MENTION = r"@[a-zA-Z0-9]{4,32}"
+LINK_RE = re.compile(r"((http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?|([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?)")
+MENTION = re.compile(r"@[a-zA-Z0-9_]{4,32}")
 
+
+def error_writer(e: Exception, f_name:str):
+    with open("error_log", "a") as f:
+        er = f_name + str(datetime.datetime.now()) + "".join(e.args)
+        f.write(er)
 
