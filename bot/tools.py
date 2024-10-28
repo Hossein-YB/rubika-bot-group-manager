e1ece8c7a21a457efdbec04ef19f24059271b5bf
Hossein <hosseinyaghoubi919@gmail.com>
2024-10-28 20:32:50 +0330

    check user permissions

--- bot/tools.py
+++ bot/tools.py
@@ -10,3 +10,24 @@ def error_writer(e: Exception, f_name: str):
         er = f_name + str(datetime.datetime.now()) + f"{e.args}"
         f.write(er)
 
+
+def user_permissions_admin(func):
+    async def check(self, msg, *args, **kwargs):
+        ch = await self.check_sender(msg, is_admin=True)
+        if not ch:
+            return False
+        f = func(self, msg, *args, **kwargs)
+        return await f
+
+    return check
+
+
+def user_permissions_creator(func):
+    async def check(self, msg, *args, **kwargs):
+        ch = await self.check_sender(msg, is_creator=True)
+        if not ch:
+            return False
+        f = func(self, msg, *args, **kwargs)
+        return await f
+
+    return check
